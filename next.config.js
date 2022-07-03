4da7e3bc25fa1c28bc7e18415ec59783d13d6ede
Thiago <thiagoalves060@gmail.com>
2022-07-03 13:06:13 -0300

    primeiro commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports ={
+ images:{
+  domains: ['storage.googleapis.com'],
+ }
 }
-
-module.exports = nextConfig
